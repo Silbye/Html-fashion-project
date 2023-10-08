1e630df9063552facc30e49da91a2f1f29b141c5
Silbye <109812796+Silbye@users.noreply.github.com>
2023-10-08 16:41:03 +0200

    Add files via upload
    Done main page cart

--- scripts/index.js
+++ scripts/index.js
@@ -17,7 +17,7 @@ async function doFeatured() {
                   class="featured-image"
                 />
                 <div class="featured-hover">
-                  <div class="featured-hover-box">
+                  <div class="featured-hover-box add-to-cart-button">
                     <svg
                       width="27"
                       height="25"
@@ -49,4 +49,79 @@ async function doFeatured() {
     }
 }
 
-doFeatured();
+function createCartItem(featured_item) {
+    featured_item.setAttribute("id", "cart-item");
+    const featured_item_title = document
+        .querySelector("#cart-item")
+        .querySelector(".featured-item-title").textContent;
+    const featured_item_price = document
+        .querySelector("#cart-item")
+        .querySelector(".featured-item-price").textContent;
+    const featured_image = document
+        .querySelector("#cart-item")
+        .querySelector(".featured-image").src;
+    console.log(featured_item);
+    console.log(featured_item_title);
+    console.log(featured_item_price);
+    console.log(featured_image);
+
+    const new_cart_item = `
+  <div class="cart-item">
+    <div class="cart-item-image">
+      <img src="${featured_image}" alt="" />
+    </div>
+    <div class="cart-item-info">
+      <div class="cart-item-title">${featured_item_title}</div>
+      <div class="cart-item-text main-page-cart-price">Price: ${featured_item_price}</div>
+      <div class="cart-item-text">Color:</div>
+      <div class="cart-item-text">Size:</div>
+      <div class="cart-item-text">Quantity:</div>
+    </div>
+    <svg class="remove-item-button" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path 
+        d="M11.2453 9L17.5302 2.71516C17.8285 2.41741 17.9962 2.01336 17.9966 1.59191C17.997 1.17045 17.8299 0.76611 17.5322 0.467832C17.2344 0.169555 16.8304 0.00177586 16.4089 0.00140366C15.9875 0.00103146 15.5831 0.168097 15.2848 0.465848L9 6.75069L2.71516 0.465848C2.41688 0.167571 2.01233 0 1.5905 0C1.16868 0 0.764125 0.167571 0.465848 0.465848C0.167571 0.764125 0 1.16868 0 1.5905C0 2.01233 0.167571 2.41688 0.465848 2.71516L6.75069 9L0.465848 15.2848C0.167571 15.5831 0 15.9877 0 16.4095C0 16.8313 0.167571 17.2359 0.465848 17.5342C0.764125 17.8324 1.16868 18 1.5905 18C2.01233 18 2.41688 17.8324 2.71516 17.5342L9 11.2493L15.2848 17.5342C15.5831 17.8324 15.9877 18 16.4095 18C16.8313 18 17.2359 17.8324 17.5342 17.5342C17.8324 17.2359 18 16.8313 18 16.4095C18 15.9877 17.8324 15.5831 17.5342 15.2848L11.2453 9Z"
+        fill="#575757"/>
+    </svg>
+  </div>`;
+    return new_cart_item;
+}
+
+function removeCartItems() {
+    const cart = document.querySelector(".main-page-cart");
+    if (document.querySelectorAll(".remove-item-button") !== null) {
+        let remove_item_buttons = document.querySelectorAll(".remove-item-button");
+        remove_item_buttons.forEach((btn) => {
+            btn.addEventListener("click", (event) => {
+                let cart_item = event.target.closest(
+                    ".remove-item-button"
+                ).parentElement;
+                cart_item.remove();
+                if (cart.querySelector(".cart-item") === null) {
+                    cart.classList.add("hidden");
+                }
+            });
+        });
+    }
+}
+
+async function mainPageCart() {
+    await doFeatured();
+    const buttons = document.querySelectorAll(".add-to-cart-button");
+    const cart_items = document.querySelector(".cart-items");
+    const cart = document.querySelector(".main-page-cart");
+    buttons.forEach((btn) => {
+        btn.addEventListener("click", (event) => {
+            let featured_item = event.target.closest(".add-to-cart-button")
+                .parentElement.parentElement.parentElement;
+            new_cart_item = createCartItem(featured_item);
+            cart_items.insertAdjacentHTML("beforeend", new_cart_item);
+            if (cart.classList.contains("hidden")) {
+                cart.classList.remove("hidden");
+            }
+            featured_item.removeAttribute("id");
+            removeCartItems();
+        });
+    });
+}
+
+mainPageCart();
